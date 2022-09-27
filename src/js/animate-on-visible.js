fc024b6a180e4e1eef36e516cbef062a710d7192
HyperTTP <59069012+HyperTTPS@users.noreply.github.com>
2022-09-28 01:24:56 +0200

    Renamed anything with 'transitions' to 'animations' and started using a different way to define the animations for elements through SCSS rather than HTML

--- src/js/animate-on-visible.js
+++ src/js/animate-on-visible.js
@@ -3,10 +3,10 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         console.log(entry);
         if (entry.isIntersecting){
-            entry.target.setAttribute("data-transition-show", "");
+            entry.target.setAttribute("data-animation-show", "");
         }
     });
 });
 
-const elements = document.querySelectorAll(".transition[data-transition-on-visible]");
+const elements = document.querySelectorAll(".animation[data-animation-play-on-visible]");
 elements.forEach(element => observer.observe(element));
